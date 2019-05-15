fd7355c5983103d2f1e9b2008ac36bb98533da6d
Igor Tkachev <tkacheviscoder@gmail.com>
2019-05-15 12:48:25 +0300

    Added routes for like & unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -105,4 +105,70 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+// @route         PUT api/posts/like/:id
+// @description   Like a post
+// @access        Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    // Check if the post has already been liked
+    const postIsLiked =
+      post.likes.filter(like => like.user.toString() === req.user.id).length >
+      0;
+    if (postIsLiked) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route         PUT api/posts/unlike/:id
+// @description   Unlike a post
+// @access        Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    // Check if the post has already been liked
+    const postIsLiked =
+      post.likes.filter(like => like.user.toString() === req.user.id).length >
+      0;
+    if (!postIsLiked) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get a new array without a "like"
+    const newLikesArr = post.likes.filter(
+      like => like.user.toString() !== req.user.id
+    );
+    post.likes = newLikesArr;
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
