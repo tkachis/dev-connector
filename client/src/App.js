9ea90238fca10f732673f69cb3ecf0c8a64a3484
Igor Tkachev <tkacheviscoder@gmail.com>
2019-05-18 16:02:05 +0300

    Protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,14 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
 // Components
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Alert from './components/layout/Alert'
-
+import Dashboard from './components/dashboard/Dashboard'
+import PrivateRoute from './components/routing/PrivateRoute'
 // *
 import store from './store'
 import setAuthToken from './helpers/setAuthToken'
@@ -36,6 +36,7 @@ const App = () => {
 						<Switch>
 							<Route exact path="/login" component={Login} />
 							<Route exact path="/register" component={Register} />
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 						</Switch>
 					</section>
 				</>
