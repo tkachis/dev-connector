471fb9451a36ab4013444e0a863f0f0cc3ad45a2
Igor Tkachev <tkacheviscoder@gmail.com>
2019-05-31 15:48:52 +0300

    Fixed date react-moment date bug

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
+import moment from 'moment'
 import { connect } from 'react-redux'
 // *
 import { deleteEducation } from '../../actions/profile'
@@ -12,11 +13,11 @@ const Education = ({ education, deleteEducation }) => {
 			<td>{edu.school}</td>
 			<td className="hide-sm">{edu.degree}</td>
 			<td>
-				<Moment format="YYYY/MM/DD">{edu.from}</Moment> -{' '}
+				<Moment format="YYYY/MM/DD">{moment.utc(edu.from)}</Moment> -{' '}
 				{edu.to === null ? (
 					' Now'
 				) : (
-					<Moment format="YYYY/MM/DD">{edu.to}</Moment>
+					<Moment format="YYYY/MM/DD">{moment.utc(edu.to)}</Moment>
 				)}
 			</td>
 			<td>
